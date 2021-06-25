33177b9af1ed65438b2323681945560d304f990e
Ian Harris <ian@fofgof.xyz>
2021-06-25 04:40:41 -0700

    iframe madness

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,29 +7,23 @@ import { dev } from '$app/env'
 let url = dev ? 'http://localhost:3000' : 'https://pleasehelpme.games'
 
 onMount(() => {
-  if (window.opener) {
-    window.opener.postMessage({
-	    message: 'cool',
-	    nonce: 1,
-    }, url)
-  }
+  window.parent.postMessage({
+    message: 'from bid',
+    nonce: 1,
+  }, url)
 
   window.addEventListener("message", (event) => {
     if (event.origin !== url)
       return;
 
-    console.log(event.data)
+    console.log({ event })
 
     // Assuming you've verified the origin of the received message (which
     // you must do in any case), a convenient idiom for replying to a
     // message is to call postMessage on event.source and provide
     // event.origin as the targetOrigin.
-    // @ts-ignore
-    event.source.postMessage({
-	    message: 'neato',
-	    nonce: 2,
-    }, event.origin);
+    event.source.postMessage('neato',
+                             event.origin);
   }, false);
-
 })
 </script>
