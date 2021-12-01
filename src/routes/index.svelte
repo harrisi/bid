6e8dc972764371607f7da3bb92047f31ac6a0979
Ian Harris <ian@fofgof.xyz>
2021-11-30 20:30:51 -0800

    use adapter-cloudflare; fix ts warning

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,16 +14,14 @@ onMount(() => {
 
   window.addEventListener("message", (event) => {
     if (event.origin !== url)
+      // handle this better
       return;
 
     console.log({ event })
 
-    // Assuming you've verified the origin of the received message (which
-    // you must do in any case), a convenient idiom for replying to a
-    // message is to call postMessage on event.source and provide
-    // event.origin as the targetOrigin.
-    event.source.postMessage('neato',
-                             event.origin);
+    event.source.postMessage('neato', {
+      targetOrigin: event.origin
+    });
   }, false);
 })
 </script>
